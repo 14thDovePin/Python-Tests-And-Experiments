4bac23e2e3c1efd946a55f132fc026691c4257b0
14thDovePin <therlaf0409@gmail.com>
2023-11-05 01:42:46 +0800

    `plotting_polygons_matplotlib` [0002]
    - Fixed `grab_random_polygon.py` indexing out of range.
    - Data structure of polygon's coordinates modified for motplotlib use.
    - Plotted polygon in matplotlib.

--- plotting_polygons_matplotlib/main.py
+++ plotting_polygons_matplotlib/main.py
@@ -6,9 +6,23 @@ object from the `shapely` library and other related test you'll see.
 
 import matplotlib.pyplot as plt
 
+from grab_random_polygon import grab_polygon
+
 
 def main():
-    pass
+    # Polygon coordinates
+    x, y = [], []
+    polygon = grab_polygon()
+
+    # Sequence all x and y coordinates respectively.
+    coordinates = polygon.exterior.coords
+    for cord in coordinates:
+        x.append(cord[0])
+        y.append(cord[1])
+
+    # Plot polygon in matplotlib.
+    plt.fill(x, y)
+    plt.show()
 
 
 if __name__ == '__main__':
