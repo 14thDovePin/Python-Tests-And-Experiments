71b45a126b2f559299a95edd68f256c3dfe18dc9
14thDovePin <therlaf0409@gmail.com>
2023-11-04 21:29:45 +0800

    Started `plotting_polygons_matplotlib`
    - Added the directory and its main file.
    - Added a breif description of the test's function and purpose.

---
+++ plotting_polygons_matplotlib/main.py
@@ -0,0 +1,12 @@
+"""
+In this test, I'll be using the `matplotlib` library to plot a polygon
+object from the `shapely` library and other related test you'll see.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
