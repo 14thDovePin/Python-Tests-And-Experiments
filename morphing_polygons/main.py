a6623dafa4bee688b8bd85d69f2195301b7e9bd5
14thDovePin <therlaf0409@gmail.com>
2023-11-11 00:05:20 +0800

    Started `morphing_polygons`
    - Added the directory and its main file.
    - Added a breif description of the test's function and purpose.
    - Added `LINKS.txt` for referencing links.

---
+++ morphing_polygons/main.py
@@ -0,0 +1,13 @@
+"""
+In this experiment, I would like to morph the polygon of Antarctica
+from Natural Earth's dataset just like in Sebastian Lague's video over
+at `LINKS.txt > [000]`.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
