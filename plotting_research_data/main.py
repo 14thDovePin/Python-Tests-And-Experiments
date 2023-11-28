b4984bdbdb91bd2f5f77146430342d22ad453397
14thDovePin <therlaf0409@gmail.com>
2023-11-28 22:37:11 +0800

    Started `plotting_research_data`
    - Added the directory and its main file.
    - Added file description, function, and purpose.
    - Added file:
      + `DATA_0.TXT`.
    - Added a basic code block.

---
+++ plotting_research_data/main.py
@@ -0,0 +1,12 @@
+"""
+The purpose of this test is to plot the data from a mockup csv file
+named `DATA_0.TXT` for 14DovePin's group research purposes.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
